3914fe9fc3c9d344d7ea70dc48435c173e8572e9
KenBoi26 <kennethphilip23@lpu.in>
2025-06-13 19:56:02 +0530

    refactor: add callback functions and bill calculation logic

--- Javascript/DAY 2/app.js
+++ Javascript/DAY 2/app.js
@@ -1,1 +1,30 @@
-console.log("Starting the app...");
+// Callbacks
+// Function that is called by the handler
+
+const print = (txt) => {
+  console.log(txt);
+};
+
+const add = (a,b) => {
+    const result = a + b;
+    return result;
+}
+
+const multiply = (a,b) => {
+    const result = a * b;
+    return result;
+}
+const ans = 10;
+
+const calculateBill = (a,b,cb) =>{
+    const ans = add(a,b);
+    const res = cb(ans,1.8);
+    return res;
+}
+
+const foodBill = (baseAmount) => {
+    return multiply(baseAmount, 1.05);
+};
+
+const result = calculateBill(400, 300, foodBill);
+print(result);
