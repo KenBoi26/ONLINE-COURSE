cefd12a6b192e11187adf4401491bb2682bec7b0
KenBoi26 <kennethphilip23@lpu.in>
2025-06-14 20:04:48 +0530

    feat: add app.js with initial DOM manipulation and user interaction

---
+++ Javascript/DAY 3 - DOM/app.js
@@ -0,0 +1,13 @@
+// APIs --> Application Programming Interface
+// DOM --> Document Object Model
+// BOM --> Browser Object Model
+// BOM is a part of the DOM
+// DOM is a tree structure of HTML elements
+
+console.log(window);
+const text = window.prompt("Enter your name");
+console.log(text);
+alert('Hello ' + text + '!');
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
